604d551c46137232563df9f28b4c9b32f5828c19
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-27 02:00:05 +0530

    footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,9 +1,45 @@
 import React from 'react'
-
+import '../css/footerComponent.css'
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
 function Footer() {
     return (
-        <div>
-            i am a footer
+        <div className="footer-container">
+            <div className="footer__left">
+                <img src="https://i.scdn.co/image/ab67616d00004851268e04235dd657681fadf9aa" className="footer__left__img" alt="" />
+                <div className="footer__left__info">
+                    <h4>Living With No One</h4>
+                    <p>Phil Good</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green footer__icon" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green footer__icon" />
+
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon className="footer__icon"/>
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon className="footer__icon" />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-labelledby="continuous-slider" />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
