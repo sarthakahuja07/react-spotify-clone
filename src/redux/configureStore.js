787f85fd0c429be51a3f821d624d0d25c470fa48
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-26 22:06:55 +0530

    whitelist token in persist

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -8,6 +8,7 @@ import storage from 'redux-persist/lib/storage' // defaults to localStorage for 
 const persistConfig = {
     key: 'root',
     storage,
+    whitelist: ['token']
 }
 
 const persistedReducer = persistReducer(persistConfig, reducer)
