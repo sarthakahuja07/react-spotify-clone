55e01f5a18968a0ca7a889f3cee3d9a59ca4fb22
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-26 18:52:11 +0530

    setting up

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBl_14GARe6cAoIgmiyQct3JRLdRYiWN14",
+    authDomain: "spotify-clone-11dd0.firebaseapp.com",
+    projectId: "spotify-clone-11dd0",
+    storageBucket: "spotify-clone-11dd0.appspot.com",
+    messagingSenderId: "799422649158",
+    appId: "1:799422649158:web:96c958ae443e5f9b0c8b47",
+    measurementId: "G-X0M2TJ9GZF"
+  };
+
+  firebase.initializeApp(firebaseConfig);
