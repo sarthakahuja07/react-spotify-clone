631beb27ed67f5f40c2e0ee2b7da44c016298d03
sarthak <sarthak.ahuja0007@gmail.com>
2021-06-27 05:35:20 +0530

    ui final commit

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,9 +6,8 @@ import SkipNextIcon from "@material-ui/icons/SkipNext";
 import ShuffleIcon from "@material-ui/icons/Shuffle";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import VolumeDownIcon from "@material-ui/icons/VolumeDown";
-import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
-import { Grid, Slider } from "@material-ui/core";
+import {  Slider } from "@material-ui/core";
 function Footer() {
     return (
         <div className="footer-container">
